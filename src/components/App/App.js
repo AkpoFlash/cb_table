c26b7d54dbeb3db6cf0540ebc49d9f939dc7681f
Kirill Anisimov <kirill.anisimov@stoloto.ru>
2019-04-02 20:31:43 +0300

    Add fetch for json

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import get from 'lodash/get';
 
 import Table from '~/components/Table/Table';
 
+// fa.fa_data.r – данные таблицы
+// fa.fa_data.axis.r – заголовки столбцов (столбцов может быть произвольное количество)
+// fDeltaPlan – значение для отображения цветового индикатора.
+
 const App = () => {
+	const [header, setHeader] = useState([]);
 	const [data, setData] = useState([]);
 
 	useEffect(() => {
-		fetch('./data.json').then(response => response.json());
+		fetch('./data.json')
+			.then(res => res.json())
+			.then(res => setHeader(get(res, 'fa.fa_data.axis.r', {})));
+
+		fetch('./data.json')
+			.then(res => res.json())
+			.then(res => setData(get(res, 'fa.fa_data.r', {})));
 	}, []);
 
 	return (
 		<>
-			<Table data={data} />
+			<Table header={header} data={data} />
 		</>
 	);
 };
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Table = () => {
-	return <></>;
+const Table = ({ header, data }) => {
+	return (
+		<table>
+			{header.map(item => (
+				<th key={item.nAxisID}>
+					<td>{item.sAxisName}</td>
+				</th>
+			))}
+		</table>
+	);
+};
+
+Table.propTypes = {
+	header: PropTypes.arrayOf(PropTypes.shape({})),
+	data: PropTypes.arrayOf(PropTypes.shape({})),
 };
 
 export default React.memo(Table);
